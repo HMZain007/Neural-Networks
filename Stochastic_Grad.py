9f0882e1c3c36090bfdbfefadf810460b2288a37
HMZain007 <zainsarwar005@gmail.com>
2021-01-03 00:25:59 +0500

    Modified

---
+++ Stochastic_Grad.py
@@ -0,0 +1,86 @@
+# Stochastic gradient descent.
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mxnet as mx
+from mxnet import autograd
+from mxnet import ndarray as nd
+from mxnet import gluon
+import random
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param[:] = param - lr * param.grad / batch_size
+
+
+data = pd.read_csv('ii.csv')
+
+# Generate data.
+num_inputs = 2
+num_examples = 1000
+true_w = [2, -3.4]
+true_b = 4.2
+X = nd.random_normal(scale=1, shape=(num_examples, num_inputs))
+y = true_w[0] * X[:, 0] + true_w[1] * X[:, 1] + true_b
+y += .01 * nd.random_normal(scale=1, shape=y.shape)
+dataset = data
+
+# Construct data iterator.
+def data_iter(batch_size):
+    idx = list(range(num_examples))
+    random.shuffle(idx)
+    for batch_i, i in enumerate(range(0, num_examples, batch_size)):
+        j = nd.array(idx[i: min(i + batch_size, num_examples)])
+        yield batch_i, X.take(j), y.take(j)
+
+# Initialize model parameters.
+def init_params():
+    w = nd.random_normal(scale=1, shape=(num_inputs, 1))
+    b = nd.zeros(shape=(1,))
+    params = [w, b]
+    for param in params:
+        param.attach_grad()
+    return params
+
+# Linear regression.
+def net(X, w, b):
+    return nd.dot(X, w) + b
+
+# Loss function.
+def square_loss(yhat, y):
+    return (yhat - y.reshape(yhat.shape)) ** 2 / 2
+
+
+
+
+def train(batch_size, lr, epochs, period):
+    assert period >= batch_size and period % batch_size == 0
+    w, b = init_params()
+    total_loss = [np.mean(square_loss(net(X, w, b), y).asnumpy())]
+    # Epoch starts from 1.
+    for epoch in range(1, epochs + 1):
+        # Decay learning rate.
+        if epoch > 2:
+            lr *= 0.1
+        for batch_i, data, label in data_iter(batch_size):
+            with autograd.record():
+                output = net(data, w, b)
+                loss = square_loss(output, label)
+            loss.backward()
+            sgd([w, b], lr, batch_size)
+            if batch_i * batch_size % period == 0:
+                total_loss.append(
+                    np.mean(square_loss(net(X, w, b), y).asnumpy()))
+        print("Epoch %d, loss %.4e" %
+              (epoch, total_loss[-1]))
+
+
+    x_axis = np.linspace(0, epochs, len(total_loss), endpoint=True)
+    plt.semilogy(x_axis, total_loss)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.show()
+
+train(batch_size=1, lr=0.1, epochs=10, period=10)
